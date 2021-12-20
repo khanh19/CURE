ebc3a0e7ff4510ba17250b4ea3cecca0173ff822
khanh19 <khanh02092000@gmail.com>
2021-12-20 18:08:47 -0500

    fix bug & splash

--- App.js
+++ App.js
@@ -2,8 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tab from "./navigation/tab";
+import * as SplashScreen from 'expo-splash-screen';
+
+
+SplashScreen.preventAutoHideAsync().then(result => console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`))
+  .catch(console.warn);
+//delay 4 seconds
+const componentDidMount = () => {
+  setTimeout(async () => {
+    await SplashScreen.hideAsync();
+  }, 4000);
+}
 
 export default function App() {
+  componentDidMount();
   return (
     // <View style={styles.container}>
     //   <Text>Hello World!</Text>
